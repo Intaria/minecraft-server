c8321abb9f9e95eb4f764f816c0b6817d40b538d
Maxim Kuznetsov <me@kaminari.pro>
2022-04-30 02:53:29 +0500

    1.18.2 update

--- src/kubejs/server_scripts/mods/alexmobs.js
+++ src/kubejs/server_scripts/mods/alexmobs.js
@@ -1,4 +1,5 @@
 //МБ в будущем выпилить что-то еще
 onEvent('recipes', event => {
-    event.remove({output: 'alexmobs:animal_dictionary'});
+    event.remove({output: 'alexsmobs:animal_dictionary'});
+    event.remove({output: 'alexsmobs:centipede_leggings'});
 })
