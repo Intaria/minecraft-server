698053a2bdac2d4c56dca9b7ad24ae80eb04b0fa
Maxim Kuznetsov <me@kaminari.pro>
2022-05-07 02:38:46 +0500

    mods updates

---
+++ src/kubejs/server_scripts/mods/oreganized.js
@@ -0,0 +1,16 @@
+//МБ в будущем выпилить что-то еще
+onEvent('recipes', event => {
+    event.remove({output: 'oreganized:electrum_block'});
+    event.remove({output: 'oreganized:electrum_ingot'});
+    event.remove({output: 'oreganized:electrum_nugget'});
+    event.remove({output: 'oreganized:electrum_sword'});
+    event.remove({output: 'oreganized:electrum_pickaxe'});
+    event.remove({output: 'oreganized:electrum_shovel'});
+    event.remove({output: 'oreganized:electrum_axe'});
+    event.remove({output: 'oreganized:electrum_hoe'});
+    event.remove({output: 'oreganized:electrum_helmet'});
+    event.remove({output: 'oreganized:electrum_chestplate'});
+    event.remove({output: 'oreganized:electrum_leggings'});
+    event.remove({output: 'oreganized:electrum_boots'});
+    event.remove({output: 'oreganized:silver_mirrow'});
+})
---
+++ src/kubejs/server_scripts/mods/plus_the_end.js
@@ -0,0 +1,12 @@
+//МБ в будущем выпилить что-то еще
+onEvent('recipes', event => {
+    event.remove({output: 'plus_the_end:endronium_sword'});
+    event.remove({output: 'plus_the_end:endronium_pickaxe'});
+    event.remove({output: 'plus_the_end:endronium_shovel'});
+    event.remove({output: 'plus_the_end:endronium_axe'});
+    event.remove({output: 'plus_the_end:endronium_hoe'});
+    event.remove({output: 'plus_the_end:endronium_helmet'});
+    event.remove({output: 'plus_the_end:endronium_chestplate'});
+    event.remove({output: 'plus_the_end:endronium_leggings'});
+    event.remove({output: 'plus_the_end:endronium_boots'});
+})
