4846387943a913ccba98b15db092b2a0db25e8df
Maxim Kuznetsov <me@kaminari.pro>
2022-05-10 16:50:31 +0500

    confir and version updates

---
+++ src/kubejs/server_scripts/mods/orcz.js
@@ -0,0 +1,10 @@
+onEvent('recipes', event => {
+    event.remove({output: 'orcz:orc_steel'});
+    event.remove({output: 'orcz:heavyorchammer'});
+    event.remove({output: 'orcz:orcteeth_tool_pickaxe'});
+    event.remove({output: 'orcz:orcteeth_tool_axe'});
+    event.remove({output: 'orcz:orcteeth_tool_sword'});
+    event.remove({output: 'orcz:orcteeth_tool_shovel'});
+    event.remove({output: 'orcz:orcteeth_tool_hoe'});
+    event.remove({output: 'orcz:orcteethpaxel'});
+})
