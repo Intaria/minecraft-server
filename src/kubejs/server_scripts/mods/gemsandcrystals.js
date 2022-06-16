73c27031e3f1e8b8e615cba40b8fbb7731d3ac3a
Maxim Kuznetsov <me@kaminari.pro>
2022-06-17 03:03:14 +0500

    add gems

---
+++ src/kubejs/server_scripts/mods/gemsandcrystals.js
@@ -0,0 +1,73 @@
+onEvent('recipes', event => {
+    event.remove({output: 'gemsandcrystals:ruby_axe'});
+    event.remove({output: 'gemsandcrystals:ruby_hoe'});
+    event.remove({output: 'gemsandcrystals:ruby_pickaxe'});
+    event.remove({output: 'gemsandcrystals:ruby_shovel'});
+    event.remove({output: 'gemsandcrystals:ruby_axe'});
+    event.remove({output: 'gemsandcrystals:ruby_sword'});
+    event.remove({output: 'gemsandcrystals:ruby_helmet'});
+    event.remove({output: 'gemsandcrystals:ruby_chestplate'});
+    event.remove({output: 'gemsandcrystals:ruby_axe'});
+    event.remove({output: 'gemsandcrystals:ruby_leggings'});
+    event.remove({output: 'gemsandcrystals:ruby_boots'});
+
+    event.remove({output: 'gemsandcrystals:sapphire_axe'});
+    event.remove({output: 'gemsandcrystals:sapphire_hoe'});
+    event.remove({output: 'gemsandcrystals:sapphire_pickaxe'});
+    event.remove({output: 'gemsandcrystals:sapphire_shovel'});
+    event.remove({output: 'gemsandcrystals:sapphire_axe'});
+    event.remove({output: 'gemsandcrystals:sapphire_sword'});
+    event.remove({output: 'gemsandcrystals:sapphire_helmet'});
+    event.remove({output: 'gemsandcrystals:sapphire_chestplate'});
+    event.remove({output: 'gemsandcrystals:sapphire_axe'});
+    event.remove({output: 'gemsandcrystals:sapphire_leggings'});
+    event.remove({output: 'gemsandcrystals:sapphire_boots'});
+
+    event.remove({output: 'gemsandcrystals:emerald_axe'});
+    event.remove({output: 'gemsandcrystals:emerald_hoe'});
+    event.remove({output: 'gemsandcrystals:emerald_pickaxe'});
+    event.remove({output: 'gemsandcrystals:emerald_shovel'});
+    event.remove({output: 'gemsandcrystals:emerald_axe'});
+    event.remove({output: 'gemsandcrystals:emerald_sword'});
+    event.remove({output: 'gemsandcrystals:emerald_helmet'});
+    event.remove({output: 'gemsandcrystals:emerald_chestplate'});
+    event.remove({output: 'gemsandcrystals:emerald_axe'});
+    event.remove({output: 'gemsandcrystals:emerald_leggings'});
+    event.remove({output: 'gemsandcrystals:emerald_boots'});
+
+    event.remove({output: 'gemsandcrystals:topaz_axe'});
+    event.remove({output: 'gemsandcrystals:topaz_hoe'});
+    event.remove({output: 'gemsandcrystals:topaz_pickaxe'});
+    event.remove({output: 'gemsandcrystals:topaz_shovel'});
+    event.remove({output: 'gemsandcrystals:topaz_axe'});
+    event.remove({output: 'gemsandcrystals:topaz_sword'});
+    event.remove({output: 'gemsandcrystals:topaz_helmet'});
+    event.remove({output: 'gemsandcrystals:topaz_chestplate'});
+    event.remove({output: 'gemsandcrystals:topaz_axe'});
+    event.remove({output: 'gemsandcrystals:topaz_leggings'});
+    event.remove({output: 'gemsandcrystals:topaz_boots'});
+
+    event.remove({output: 'gemsandcrystals:amethyst_axe'});
+    event.remove({output: 'gemsandcrystals:amethyst_hoe'});
+    event.remove({output: 'gemsandcrystals:amethyst_pickaxe'});
+    event.remove({output: 'gemsandcrystals:amethyst_shovel'});
+    event.remove({output: 'gemsandcrystals:amethyst_axe'});
+    event.remove({output: 'gemsandcrystals:amethyst_sword'});
+    event.remove({output: 'gemsandcrystals:amethyst_helmet'});
+    event.remove({output: 'gemsandcrystals:amethyst_chestplate'});
+    event.remove({output: 'gemsandcrystals:amethyst_axe'});
+    event.remove({output: 'gemsandcrystals:amethyst_leggings'});
+    event.remove({output: 'gemsandcrystals:amethyst_boots'});
+
+    event.remove({output: 'gemsandcrystals:quartz_axe'});
+    event.remove({output: 'gemsandcrystals:quartz_hoe'});
+    event.remove({output: 'gemsandcrystals:quartz_pickaxe'});
+    event.remove({output: 'gemsandcrystals:quartz_shovel'});
+    event.remove({output: 'gemsandcrystals:quartz_axe'});
+    event.remove({output: 'gemsandcrystals:quartz_sword'});
+    event.remove({output: 'gemsandcrystals:quartz_helmet'});
+    event.remove({output: 'gemsandcrystals:quartz_chestplate'});
+    event.remove({output: 'gemsandcrystals:quartz_axe'});
+    event.remove({output: 'gemsandcrystals:quartz_leggings'});
+    event.remove({output: 'gemsandcrystals:quartz_boots'});
+})
