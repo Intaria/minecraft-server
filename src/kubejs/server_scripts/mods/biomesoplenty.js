13359f2ab6175646233687a35eba364daed65001
Maxim Kuznetsov <me@kaminari.pro>
2022-06-29 16:17:18 +0500

    part of updates

--- src/kubejs/server_scripts/mods/biomesoplenty.js
+++
@@ -1,4 +0,0 @@
-onEvent('recipes', event => {
-    event.remove({output: 'biomesoplenty:origin_leaf_carpet'});
-    event.remove({output: 'biomesoplenty:origin_hedge'});
-})
--- src/kubejs/server_scripts/mods/drinkbeer.js
+++
@@ -1,3 +0,0 @@
-onEvent('recipes', event => {
-    event.remove({output: 'drinkbeer:recipe_board_package'});
-})
