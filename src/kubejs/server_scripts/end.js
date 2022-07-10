ac13a7611dca44da8bfd78f92af7aa5fd036a5aa
Maxim Kuznetsov <me@kaminari.pro>
2022-07-11 03:49:36 +0500

    some usefull coments

--- src/kubejs/server_scripts/end.js
+++ src/kubejs/server_scripts/end.js
@@ -1,4 +1,5 @@
 onEvent('block.right_click', event => {
+    //Добавить проверку, что отменяется только если в руке глаз эндера
     if (event.block.id == 'minecraft:end_portal_frame'){
         event.cancel()
     }
