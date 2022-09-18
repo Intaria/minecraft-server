6393cbbe72d587becd5622bc3655d65f519a3563
Maxim Kuznetsov <me@kaminari.pro>
2022-09-19 04:52:32 +0500

    update mods

---
+++ src/kubejs/server_scripts/mods/ecologics.js
@@ -0,0 +1,5 @@
+onEvent('recipes', event => {
+    event.remove({output: 'ecologics:pot'});
+    event.remove({output: 'ecologics:crab_claw'});
+    event.remove({output: 'ecologics:crab_meat'});
+})
