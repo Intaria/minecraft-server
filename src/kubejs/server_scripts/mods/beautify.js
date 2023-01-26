6fde84a40547887c0bcd4ca86e0fa4c38fcd5917
Maxim Kuznetsov <me@kaminari.pro>
2023-01-27 01:33:13 +0500

    update rope recipe

--- src/kubejs/server_scripts/mods/beautify.js
+++ src/kubejs/server_scripts/mods/beautify.js
@@ -18,6 +18,15 @@ ServerEvents.recipes(event => {
     event.remove({output: 'beautify:lamp_candleabra_red'});
     event.remove({output: 'beautify:lamp_candleabra_white'});
     event.remove({output: 'beautify:lamp_candleabra_yellow'});
+
+    event.remove({output: 'beautify:hanging_pot'});
+
+    event.shaped('beautify:hanging_pot', [
+        'R',
+        'P',
+    ], {
+        R: 'supplementaries:rope', P: 'minecraft:flower_pot',
+    })
 })
 
  LootJS.modifiers((event) => {
--- src/kubejs/server_scripts/mods/supplementaries.js
+++ src/kubejs/server_scripts/mods/supplementaries.js
@@ -1,8 +1,8 @@
 ServerEvents.recipes(event => {   
     event.shaped('supplementaries:rope', [
-        ' S ',
-        ' S ',
-        ' S '
+        'S',
+        'S',
+        'S'
     ], {
         S: 'farmersdelight:straw'
     });
